3d5e5afa1266bcaeaf7155b7d4acd6c9222e8427
RealJavaDoe <43642666+RealJavaDoe@users.noreply.github.com>
2019-02-26 10:41:42 +0100

    Update MyPuzzle.java

--- classes/MyPuzzle.java
+++ classes/MyPuzzle.java
@@ -9,7 +9,7 @@ import javax.swing.border.Border;
 /**
  * @author Jean-Pierre PEIFFER
  * @edition 2019
- * @version 1.0
+ * @version 1.00
  * 
  *          This code works fine with Windows 10 and Java 10
  */
