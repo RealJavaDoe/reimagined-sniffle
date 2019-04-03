7d26ddf12ab432485a232beeccb9050e15d0ca89
Jean-Pierre PEIFFER <43642666+RealJavaDoe@users.noreply.github.com>
2019-04-03 13:43:03 +0200

    Update MyCalendar.java

--- classes/MyCalendar.java
+++ classes/MyCalendar.java
@@ -39,7 +39,7 @@ public class MyCalendar {
 	private static final String[] TEXT_BUTTON = { "\u22b2 Prev.", "Now", "Next \u22b3" };
 	private static final String[] TEXT_MONTH = { "January", "February", "March", "April", "May", "June", "July",
 			"August", "September", "October", "November", "December" };
-	private static final String TITLE = "Calendar";
+	private static final String NAME = "Calendar";
 	private static final Calendar CALENDAR = Calendar.getInstance();
 	private static final int CURRENT_DAY = CALENDAR.get(Calendar.DAY_OF_MONTH);
 	private static final int CURRENT_MONTH = CALENDAR.get(Calendar.MONTH);
@@ -232,7 +232,7 @@ public class MyCalendar {
 
 	public void start(int month, int year) {
 		frame = new JFrame();
-		frame.setTitle(TITLE);
+		frame.setTitle(NAME);
 		frame.setSize(DIM_2);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
